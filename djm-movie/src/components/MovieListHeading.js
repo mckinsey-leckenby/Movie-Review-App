0ed21d1ae1e87bae538361048e294754fa04c131
Jeremiah <jeremiahmarcos27@outlook.com>
2021-12-10 08:42:14 -0600

    updated

--- djm-movie/src/components/MovieListHeading.js
+++ djm-movie/src/components/MovieListHeading.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 const MovieListHeading = (props) => {
+    const history = useHistory()
+
+    const handleClick = () => {
+        history.push("/")
+    }
+
     return (
-        <div className='col'>
+        <div className='col' onClick={handleClick}>
             <h1>{props.heading}</h1>
         </div>
     )
