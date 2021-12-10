4066a8612b6a6bf213eaf53c57a861d251815b7a
Jeremiah <jeremiahmarcos27@outlook.com>
2021-12-10 08:53:05 -0600

    added include reviews for movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,41 +1,38 @@
 class MoviesController < ApplicationController
-    
-  get "/movies" do
+  get '/movies' do
     movies = Movie.all
-    movies.to_json
+    movies.to_json(include: :reviews)
   end
 
-  get "/movies/:id" do 
+  get '/movies/:id' do
     movie = Movie.find(params[:id])
     movie.to_json(include: :reviews)
   end
 
-  get "/movies/:id/reviews" do 
+  get '/movies/:id/reviews' do
     movie = Movie.find(params[:id]).reviews
     movie.to_json
   end
 
   post '/movies/:id/reviews' do
-    review = Review.create(
-      rating: params[:rating],
-      comment: params[:comment],
-      movie_id: params[:movie_id],
-      user_id: params[:user_id]
-    )
+    review =
+      Review.create(
+        rating: params[:rating],
+        comment: params[:comment],
+        movie_id: params[:movie_id],
+        user_id: params[:user_id],
+      )
     review.to_json
   end
-  patch '/movies/:id/reviews' do 
+  patch '/movies/:id/reviews' do
     review = Review.find(params[:id])
-    review.update(
-      comment: params[:comment],
-      rating: params[:rating]
-    )
+    review.update(comment: params[:comment], rating: params[:rating])
     review.to_json
   end
 
-  delete "/movies/:id/reviews" do
+  delete '/movies/:id/reviews' do
     review = Review.find(params[:id])
     review.destroy
-    {message:'review destroyed'}.to_json
+    { message: 'review destroyed' }.to_json
   end
 end
