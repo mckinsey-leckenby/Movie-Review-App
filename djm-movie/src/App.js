eb2a9beddf338ed52da47da0f2c85f633b7286bf
Jeremiah <jeremiahmarcos27@outlook.com>
2021-12-10 09:47:33 -0600

    added buttons

--- djm-movie/src/App.js
+++ djm-movie/src/App.js
@@ -8,9 +8,10 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
+  Link
 } from "react-router-dom";
 import Home from './components/Home';
-
+import Button from 'react-bootstrap/Button'
 
 const App = () => {
   const [movies, setMovies] = useState([]);
@@ -24,12 +25,19 @@ const App = () => {
     fetch(shows_url).then(r => r.json()).then(setShows)
   }, []);
 
-
+  const handleClick = () => {
+    window.location.reload(false)
+  }
 
   return (
     <>
       <div>
         <MovieListHeading heading='Fresh Potato' />
+        <div className='movie-or-shows' onClick={handleClick}>
+          <Link to='/movies'><Button>Movies</Button></Link><br />
+          <Link to='/shows'><Button>Shows</Button></Link>
+        </div>
+
         <Router>
           <Switch>
             <Route exact path="/">
